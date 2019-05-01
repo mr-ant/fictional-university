adf506e5f7f5ded6a797c4208d4f6246d52f5eb5
jesse <pzf_karl@hotmail.com>
2019-05-01 22:31:41 +0800

    parent and child pages

--- page.php
+++ page.php
@@ -15,17 +15,39 @@
 
   <div class="container container--narrow page-section">
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-    </div>
+    <?php 
+        $parentID = wp_get_post_parent_id(get_the_ID());
+        if ($parentID) { ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($parentID) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parentID); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+            </div>
+        <?php } 
+    ?>
+
     
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+    <?php 
+    $hasChildren = get_pages(array(
+        'child_of' => get_the_ID()
+    ));
+    if ($parentID or $hasChildren) { //仅当该页面有父页面或子页面显示该导航 ?> 
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($parentID) ?>"><?php echo get_the_title($parentID); ?></a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+        <?php 
+            if ($parentID) {
+                $findChildrenOf = $parentID;
+            } else {
+                $findChildrenOf = get_the_ID();
+            }
+            wp_list_pages(array(
+                'title_li' => NULL,
+                'child_of' => $findChildrenOf,
+                'sort_column' => 'menu_order' // 设置页面排序。
+            )); 
+        ?>
       </ul>
-    </div> -->
+    </div>
+        <?php }?>
 
     <div class="generic-content">
         <?php the_content();?>
