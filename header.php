36ad1e6e96a34f12ea60eb0aeaeafdaee8fe742a
jesse <pzf_karl@hotmail.com>
2019-05-02 10:13:28 +0800

    improvments

--- header.php
+++ header.php
@@ -1,35 +1,37 @@
 <!DOCTYPE html>
-<html <?php language_attributes();?>>
+<html <?php language_attributes(); ?>>
+
 <head>
-    <meta charset="<?php bloginfo('charset');?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
-    <?php wp_head();?>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
+  <?php wp_head(); ?>
 </head>
-<body <?php body_class()?>>
-<header class="site-header">
+
+<body <?php body_class() ?>>
+  <header class="site-header">
     <div class="container">
       <h1 class="school-logo-text float-left"><a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a></h1>
       <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
-        <!-- <?php wp_nav_menu(array(
-    'theme_location' => 'headerMenuLocation',
-));
-?> -->
+          <!-- <?php wp_nav_menu(array(
+                  'theme_location' => 'headerMenuLocation',
+                ));
+                ?> -->
           <ul>
             <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 10) {
-    echo 'class="current-menu-item"';
-}
-?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
+                  echo 'class="current-menu-item"';
+                }
+                ?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
             <li><a href="#">Programs</a></li>
             <li><a href="#">Events</a></li>
             <li><a href="#">Campuses</a></li>
-            <li  <?php if (is_page('/blog')) {
-    echo 'class="current-menu-item"';
-}
-?>><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
+            <li <?php if (get_post_type() == 'post') {
+                  echo 'class="current-menu-item"';
+                }
+                ?>><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
           </ul>
         </nav>
         <div class="site-header__util">
@@ -40,4 +42,3 @@
       </div>
     </div>
   </header>
-
