c36519abc3102396a4f324f0fcbef2457e5ad043
Jesse Peng <pzf_karl@hotmail.com>
2019-05-03 12:05:39 +0800

    setup custom post type event

--- functions.php
+++ functions.php
@@ -17,3 +17,25 @@ function taotaomeow_features()
 }
 
 add_action('after_setup_theme', 'taotaomeow_features');
+
+// 自定义文章类型
+
+function ttm_post_types()
+{
+    register_post_type('event', array(
+        'supports' => array('title', 'editor', 'excerpt'),
+        'rewrite' => array('slug' => 'events'),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Events',
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event'
+        ),
+        'menu_icon' => 'dashicons-calendar'
+    ));
+}
+
+add_action('init', 'ttm_post_types');
