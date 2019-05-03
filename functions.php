1c4cb974a696e1552b4991f669685f45014fbb75
Jesse Peng <pzf_karl@hotmail.com>
2019-05-03 14:43:57 +0800

    add custom field with ACF

--- functions.php
+++ functions.php
@@ -23,7 +23,7 @@ add_action('after_setup_theme', 'taotaomeow_features');
 function ttm_post_types()
 {
     register_post_type('event', array(
-        'supports' => array('title', 'editor', 'excerpt'),
+        'supports' => array('title', 'editor', 'excerpt', 'custom-fields'),
         'rewrite' => array('slug' => 'events'),
         'has_archive' => true,
         'public' => true,
