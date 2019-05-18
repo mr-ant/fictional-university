7d4266ec8718e78446b5e410722fc6919c30c2ee
jesse <pzf_karl@hotmail.com>
2019-05-18 20:54:17 +0800

    update advanced custom fields

--- Dr.Meow/trunk/functions.php
+++ Dr.Meow/trunk/functions.php
@@ -232,6 +232,49 @@ if (function_exists('acf_add_local_field_group')) :
     ));
 
     acf_add_local_field_group(array(
+        'key' => 'group_5cdff715e3281',
+        'title' => 'Main Body Content',
+        'fields' => array(
+            array(
+                'key' => 'field_5cdffb40e584b',
+                'label' => 'Main Body Content',
+                'name' => 'main_body_content',
+                'type' => 'wysiwyg',
+                'instructions' => '',
+                'required' => 1,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'tabs' => 'all',
+                'toolbar' => 'full',
+                'media_upload' => 1,
+                'delay' => 0,
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'program',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'acf_after_title',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
+
+    acf_add_local_field_group(array(
         'key' => 'group_5cccdd9650c55',
         'title' => 'Map Location',
         'fields' => array(
