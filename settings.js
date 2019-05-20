19a4eadf5f4f07b50e46c218b80507cd8fddaf53
Jesse Peng <pzf_karl@hotmail.com>
2019-05-20 19:06:49 +0800

    modify theme lacation because of docker

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
-exports.themeLocation = './';
-exports.urlToPreview = 'http://ttm.local';
+exports.themeLocation = './Dr.Meow/trunk/';
+exports.urlToPreview = 'http://ttmeow.local';
 
 // If you're using Local by Flywheel you will
 // want your settings to be similar to the examples below:
