f58752ee2624316a61fc1efcea5a7e1f24e72a6d
Jesse Peng <pzf_karl@hotmail.com>
2019-05-21 09:54:39 +0800

    customize login form

--- Dr.Meow/trunk/functions.php
+++ Dr.Meow/trunk/functions.php
@@ -529,3 +529,25 @@ function removeSubsAdminBar()
         show_admin_bar(false);
     }
 }
+
+// customize login form
+
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderUrl()
+{
+    return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+function ourLoginCSS()
+{
+    wp_enqueue_style('taotaomeow', get_stylesheet_uri(), null, microtime());
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle()
+{
+    return get_bloginfo('name');
+}
