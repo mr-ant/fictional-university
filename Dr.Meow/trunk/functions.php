bd4cd1a4c49e551e38db449708a23b29de98cc6f
Jesse Peng <pzf_karl@hotmail.com>
2019-05-22 13:10:47 +0800

    add author to note post type, fix my note problem.

--- Dr.Meow/trunk/functions.php
+++ Dr.Meow/trunk/functions.php
@@ -186,7 +186,7 @@ function ttm_post_types()
     // Note Post Type
     register_post_type('note', array(
         'show_in_rest' => true,
-        'supports' => array('title', 'editor'),
+        'supports' => array('title', 'editor', 'author'),
         'public' => false,
         'show_ui' => true,
         'labels' => array(
