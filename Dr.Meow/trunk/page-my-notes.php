83285d5421d890cea373afdd3dd90dc18d2a18aa
jesse <pzf_karl@hotmail.com>
2019-05-24 17:36:45 +0800

    update,delete,create note frontend

--- Dr.Meow/trunk/page-my-notes.php
+++ Dr.Meow/trunk/page-my-notes.php
@@ -13,6 +13,14 @@ while (have_posts()) {
     ?>
 
     <div class="container container--narrow page-section">
+
+        <div class="create-note">
+            <h2 class="headline headline--medium">Create New Note</h2>
+            <input type="text" placeholder="Title" class="new-note-title">
+            <textarea class="new-note-body" placeholder="Your note here..."></textarea>
+            <span class="submit-note">Create Note</span>
+        </div>
+
         <ul class="min-list link-list" id="my-notes">
             <?php
             $userNotes = new WP_Query(array(
@@ -24,10 +32,11 @@ while (have_posts()) {
             while ($userNotes->have_posts()) {
                 $userNotes->the_post(); ?>
                 <li data-id="<?php the_ID(); ?>">
-                    <input class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
+                    <input readonly class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
                     <span class="edit-note"><i class="fas fa-edit" aria-hidden="true"></i> Edit</span>
                     <span class="delete-note"><i class="fas fa-trash-alt" aria-hidden="true"></i> Delete</span>
-                    <textarea class="note-body-field"><?php echo esc_textarea(get_the_content()); ?></textarea>
+                    <textarea readonly class="note-body-field"><?php echo esc_attr(get_the_content()); ?></textarea>
+                    <span class="update-note btn btn--blue btn--small"><i class="fas fa-arrow-right" aria-hidden="true"></i> Save </span>
                 </li>
             <?php }
 
